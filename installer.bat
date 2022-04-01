208c9337fb373c8d9208c1f20b19a5926169391c
saidgc <said.g@hotmail.com>
2022-03-31 22:04:33 -0600

    perf: improves

--- installer.bat
+++ installer.bat
@@ -1,24 +1,45 @@
 @ECHO OFF
 set actual_dir=%cd%
 
-ECHO Verificando sistema
+$sb_service_path = "C:\SB Service\"
+$sb_service = "SmartBambooApiService"
+$sb_api = "sb_api_service.py"
+
+$sf_systems =  "C:\SF Systems\"
+
 POWERSHELL -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco feature enable -n=allowGlobalConfirmation
 
-ECHO Instalando requeriminentos
 choco install python
 choco install nssm
 choco install 7zip.portable
 
 py -m pip install fastapi uvicorn python-multipart
 
-nssm stop "SmartBambooApiService"
-nssm remove "SmartBambooApiService" confirm
-nssm install "SmartBambooApiService" "py" "%actual_dir%\sb_api_service.py"
-nssm start "SmartBambooApiService"
 
-if not exist "C:\SF Systems\" (
-  7z x installer.zip -o"C:\"
-) 
+if not exist $sf_systems (
+    mkdir $sf_systems
+    7z x RelaySinPantallas.zip -o$sf_systems
+)
+
+if (Get-Service "$sb_service" -ErrorAction SilentlyContinue) {
+  $running = Get-Service $sb_service
+  if ($running.Status -eq "Running") {
+    nssm stop $sb_service
+    nssm remove $sb_service confirm
+  }
+}
+
+if not exist $sb_service_path (
+    mkdir $sb_service_path
+    copy "%actual_dir%\$sb_api" $sb_service_path
+) else (
+    del "$sb_service_path\$sb_api"
+    copy "%actual_dir%\$sb_api" $sb_service_path
+)
+
+nssm install $sb_service "py" "$sb_service_path\sb_api_service.py"
+nssm start $sb_service
+
 
 PAUSE
