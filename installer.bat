0761674e3bb9078526fc5ace040dec3324dfdca4
Said Gonzalez Conde <said.g@hotmail.com>
2023-08-18 16:20:27 +0000

    feat: update installer

--- installer.bat
+++ installer.bat
@@ -15,7 +15,7 @@ set sf_systems="C:\SF Systems\"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco feature enable -n allowGlobalConfirmation
 choco upgrade chocolatey
-choco upgrade all
+:: choco upgrade all
 
 choco install python --pre
 choco install nssm
